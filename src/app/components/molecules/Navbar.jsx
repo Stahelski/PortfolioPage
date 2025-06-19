5f5ec8fed4e44d1f9bf5e805cf302305ea06c047
stian <stiankarlsen709@gmail.com>
2025-06-19 14:51:02 +0200

    hero, pricing edit

--- src/app/components/molecules/Navbar.jsx
+++ src/app/components/molecules/Navbar.jsx
@@ -10,35 +10,33 @@ export default function Navbar() {
     <>
       <div className="Navbar z-[5] h-[7.8dvh] w-full flex justify-around items-center md:px-38 rounded-[27px_27px_0_0] ">
         <Link
-          href="/aboutPage"
+          href="/"
           className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl"
         >
           Pricing
         </Link>
 
-       
-          <Link
-            href="/contactPage"
-            className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl"
-          >
-            Contact
-          </Link>
-          <Link
-            href="/aboutPage"
-            className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl"
-          >
-            About
-          </Link>
+        <Link
+          href="/contactPage"
+          className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl"
+        >
+          Contact
+        </Link>
+        <Link
+          href="/aboutPage"
+          className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl"
+        >
+          About
+        </Link>
 
-          <a
-            href="https://github.com/Stahelski"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl"
-          >
-            Github
-          </a>
-        
+        <a
+          href="https://github.com/Stahelski"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl"
+        >
+          Github
+        </a>
       </div>
     </>
   );
