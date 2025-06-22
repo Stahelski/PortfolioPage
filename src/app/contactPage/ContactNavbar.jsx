434bb396d8ca94123699f006aa1cf9e9b285820e
stian <stiankarlsen709@gmail.com>
2025-06-22 15:04:09 +0200

    contact page update

--- src/app/contactPage/ContactNavbar.jsx
+++ src/app/contactPage/ContactNavbar.jsx
@@ -5,7 +5,7 @@ export default function ContactNavbar() {
     <>
       <div className="Navbar z-[5] h-[7.8dvh] w-full flex justify-around items-center rounded-[27px_27px_0_0] md:px-38">
         <a
-          className="cursor-pointer text-md sm:text-2xl md:text-3xl xl:text-4xl text-amber-50"
+          className="text-md sm:text-2xl md:text-3xl xl:text-4xl text-amber-50"
         >
           Send me an Email
         </a>
--- src/app/contactPage/page.jsx
+++ src/app/contactPage/page.jsx
@@ -16,19 +16,19 @@ export default function contactPage() {
         <h1>FrontendForge</h1>
       </Link>
 
-      <div className="ContactPageCard h-2/3 w-3/4 z-[5] mt-12 flex flex-col bg-white/5 backdrop-blur-sm">
+      <div className="ContactPageCard h-2/3 w-5/9 z-[5] mt-12 flex flex-col bg-white/5 backdrop-blur-sm">
         <ContactNavbar />
         {/* <form>: Selve skjemaet. action angir hvor dataene skal sendes, og method="POST" brukes for å sende data sikkert. */}
         <form
-          className="w-full h-full flex justify-center items-center"
+          className="w-full h-full flex flex-col sm:flex-row justify-center items-center gap-6"
           action="/send-message"
           method="POST"
         >
-          <div className="Left bg-transparent w-1/2 flex flex-col">
-            <div className="NameFeeld flex justify-start items-center">
+          <div className="Left w-full md:w-1/2 flex flex-col justify-center items-center gap-4">
+            <div className="NameFeeld w-full ml-5 flex justify-center sm:justify-end items-center">
               <img className="h-8 w-8 mr-1" src="/user.png" alt="user icon" />
               <input
-                className="Name w-20 h-8 text-amber-50 bg-transparent border-b-2 border-white outline-none pt-3"
+                className="Name w-64 h-8 text-amber-50 border-b-2 border-white outline-none pt-3"
                 type="text"
                 id="name"
                 placeholder="Name:"
@@ -36,10 +36,10 @@ export default function contactPage() {
               ></input>
             </div>
 
-            <div className="EmailFeeld flex justify-start items-center">
+            <div className="EmailFeeld w-full h-30 ml-5 flex justify-center sm:justify-end items-center">
               <img className="w-8 h-8 mr-1" src="/Epost.svg" alt="email icon" />
               <input
-                className="Email w-20 h-8 text-amber-50 bg-transparent border-b-2 border-white outline-none pt-3"
+                className="Email w-64 h-8 text-amber-50 border-b-2 border-white outline-none pt-3"
                 type="email"
                 id="email"
                 name="email"
@@ -48,10 +48,10 @@ export default function contactPage() {
               ></input>
             </div>
 
-            <div className="PhoneFeeld flex justify-start items-center">
+            <div className="PhoneFeeld w-full ml-5 flex justify-center sm:justify-end items-center gap-4">
               <img className="w-8 h-8 mr-1" src="/phone.png" alt="phone icon" />
               <input
-                className="Phone w-20 h-8 text-amber-50 bg-transparent border-b-2 border-white outline-none pt-3"
+                className="Phone w-64 h-8 text-amber-50 border-b-2 border-white outline-none pt-3"
                 type="num"
                 id="phone"
                 name="phone"
@@ -60,16 +60,19 @@ export default function contactPage() {
             </div>
           </div>
 
-          <div className="w-1/2 h-1/1 flex flex-col items-center justify-center">
+          <div className="Right w-full sm:w-1/2 h-full m-10 flex flex-col items-end justify-center">
             <textarea
-              className="Right w-5/6 h-1/2 p-2 text-white bg-white/25 rounded-2xl outline-none"
+              className="w-full h-60 p-2 mt-[8%] text-white bg-white/25 rounded-2xl outline-none"
               id="message"
               name="message"
               placeholder="Message.."
               required
             ></textarea>
 
-            <button className="top-17 right-12 text-white" type="submit">
+            <button
+              className="cursor-pointer px-6 py-2 m-2 bg-white/25 hover:bg-white/40 text-white rounded-[0_27px_27px_27px] transition"
+              type="submit"
+            >
               Send
             </button>
           </div>
