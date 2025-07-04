1e32393f8a3cdd2c33d172ac5c67ac7457caec73
stian <stiankarlsen709@gmail.com>
2025-07-04 22:56:04 +0200

    file

---
+++ src/app/OnePagersMyArt/LookingGlass/page.jsx
@@ -0,0 +1,3 @@
+// A page you are looking thrue a window, there is some mist on the window and the sunn is shining through.
+// The background is lush green with a modern house with bigg glass windows.
+// You interact with the window and its like a finger touching the glass and interact with the mist.
