9aa462971c2fc8843b54dda8c8c36f4147d4359b
stian <stiankarlsen709@gmail.com>
2025-07-05 00:25:13 +0200

    file

--- src/app/OnePagersMyArt/Romulus/page.jsx
+++ src/app/OnePagersMyArt/Romulus/page.jsx
@@ -3,3 +3,4 @@
 // Svart bakgrunnd, med et pixelert romulus logo øverst til venstre, med en pixelert navbar Grønn skrift.
 // Et Pixelert bilde av Skipet Romulus som svever i mørket, det tar opp 40% av skjermens høyde fra bunnen av, og fyller skjermen horisontalt.
 // Til venstre over skjermen en fet pixelert skrift som sier "The road to making life multiplanetary"
+// når du skroller ned på siden får du en excel lignende liste med kolonner for "ID nummer. Vessel Type/name. Length. Width. Beam. Draft. DWT. BHP. BP. Class. Location. Price."
