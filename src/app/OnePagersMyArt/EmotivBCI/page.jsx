4d2c68b48b72c6432ac2a6019309c8bcb22031ce
stian <stiankarlsen709@gmail.com>
2025-07-05 22:32:37 +0200

    Pika

--- src/app/OnePagersMyArt/EmotivBCI/page.jsx
+++ src/app/OnePagersMyArt/EmotivBCI/page.jsx
@@ -3,9 +3,3 @@
 // Altså nettsider og apper du kan bruke med tankene dine
 
 // https://www.youtube.com/@EmotivAcademy/videos
-
-// Salgs pitch
-// Hvorfor blir ikke dette brukt? Utstyret er tilgjengelig og ikke så dyrt.
-// Fordi ingen har tilrettelagt internett til det.
-// Jeg/vi kan bli noen av di første som tilrettelegger et internett styrt med tanker!
-//
---
+++ src/app/OnePagersMyArt/MikaPikazo/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MikaPikazoPage() {
+  return (
+    <>
+      <div className="BG">
+        <div className="navbar"></div>
+        <div className="sidebar"></div>
+      </div>
+    </>
+  );
+}
