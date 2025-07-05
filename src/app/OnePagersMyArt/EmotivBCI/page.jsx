32bf917200cb8dd9ae2c2521fb81e149c3e4b63e
stian <stiankarlsen709@gmail.com>
2025-07-05 19:06:03 +0200

    EmotivBCI

---
+++ src/app/OnePagersMyArt/EmotivBCI/page.jsx
@@ -0,0 +1,5 @@
+// Hva skal målet til prosjektet være?
+// # Lage nettsider som er kompatibel med Emotiv BCI EEG Brain interface
+// Altså nettsider og apper du kan bruke med tankene dine
+
+// https://www.youtube.com/@EmotivAcademy/videos
--- src/app/OnePagersMyArt/LookingGlass/page.jsx
+++ src/app/OnePagersMyArt/LookingGlass/page.jsx
@@ -1,3 +1,19 @@
-// A page you are looking thrue a window, there is some mist on the window and the sunn is shining through.
-// The background is lush green with a modern house with bigg glass windows.
-// You interact with the window and its like a finger touching the glass and interact with the mist.
+// A page where you are looking through a window. There are some raindrops on the window and the sun is shining through.
+// The background is blurry because of the drops, and it's a lush green scene with a modern house with big glass windows.
+// You interact with the window as if a finger is touching the glass and interacting with the drops.
+
+// Explaining how a window looks from the inside:
+// You see through the window, but you also see some sort of slightly white reflection depending on the interior—usually angular.
+
+// Explaining how the drops look on the window:
+// The background is blurry because of the drops.
+// The individual drops show the color of the background shining through, upside down (usually darker on the top, lighter and clearer on the bottom).
+
+// Explaining how the interaction looks:
+// The mouse is a transparent circle with a whitish border.
+// When you drag the mouse over the screen, it leaves a trail of smaller drops,
+// and collects the bigger drops until there is too much water surrounding the circle,
+// causing drops to roll down the window—interacting with each drop on their way down and leaving a clear transparent line behind.
+
+// Pseudocode
+// Create a window/plane of glass – Use a 3D canvas.
