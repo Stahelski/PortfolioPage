e6d3d2358092de610f443ef1e27622994b252a24
stian <stiankarlsen709@gmail.com>
2025-07-20 01:05:47 +0200

    Big update, new front page added, new project page added, update on about page

--- src/app/NewHero/page.jsx
+++ src/app/NewHero/page.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { Toaster, toast } from "react-hot-toast";
+import { MapPin } from "lucide-react";
 import HoneyComb from "../components/atoms/HexBackground";
 
 export default function NewHero() {
@@ -45,13 +46,13 @@ export default function NewHero() {
       {/* Main Content Grid */}
       <div className="z-10 h-[65vh] w-[52vw] grid grid-cols-7 grid-rows-16 gap-3">
         {/* About Text */}
-        <div className="col-span-3 row-span-4 bg-[#1f1d22] text-stone-200 rounded-md flex items-center pr-2 pl-4">
-          I'm Stian, a front-end web developer located in Norway.
+        <div className="col-span-3 row-span-4 bg-[#1f1d22] text-stone-300 rounded-md flex items-center pr-2 pl-4">
+          I'm Stian, a front-end developer with a focus on React and Next.js.
         </div>
 
         {/* Tech Stack */}
         <div className="col-span-3 row-start-5 row-span-9 bg-[#1f1d22] rounded-md px-1 grid grid-cols-4 grid-rows-7 gap-0">
-          <div className="col-span-4 flex items-center text-stone-200 pl-2 pt-2">
+          <div className="col-span-4 flex items-center text-stone-300 pl-2 pt-2">
             Tech stack
           </div>
           {techStack.map((tech, index) => (
@@ -65,16 +66,16 @@ export default function NewHero() {
         </div>
 
         {/* Email */}
-        <div className="relative col-span-3 row-start-14 row-end-16 justify-center bg-[#1f1d22] text-stone-200 rounded-md flex items-center pr-4">
+        <div className="relative col-span-3 row-start-14 row-end-16 justify-center bg-[#1f1d22] text-stone-300 rounded-md flex items-center pr-4">
           Stiankarlsen709@gmail.com
           <button
-            className="absolute right-0 cursor-pointer transform hover:scale-112 transition-transform duration-75"
+            className="absolute right-0 cursor-pointer transform hover:scale-112 transition-transform duration-75 "
             onClick={kopier}
           >
             <img
               src="/copy_all_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png"
               alt="copy image"
-              className="right-0 pr-1 h-4"
+              className="right-0 pr-1 h-4 "
             ></img>
           </button>
         </div>
@@ -92,11 +93,11 @@ export default function NewHero() {
 
           {/* Line Divider */}
           <div className="row-start-3 col-start-2 col-end-10 flex items-center">
-            <div className="bg-stone-200 h-[2px] w-full" />
+            <div className="bg-stone-300 h-[2px] w-full" />
           </div>
 
           {/* Image */}
-          <div className="relative row-start-4 row-end-8 col-start-2 col-end-6 flex justify-center items-center group">
+          <div className="relative row-start-4 row-end-8 col-start-2 col-end-6 mb-2 flex justify-center items-center group">
             <img
               src="/ImgMe.jpg"
               alt="Image of me"
@@ -110,9 +111,11 @@ export default function NewHero() {
             />
           </div>
 
-          <div className="row-start-2 col-start-8 col-end-10 flex justify-around items-center bg-[#1f1d22]">
+          <div className="relative row-start-2 col-start-2 col-end-10 mt-3 flex justify-end items-center bg-[#1f1d22]">
+            <MapPin className="absolute left-0 mt-3 w-4 h-4 text-sky-400" />
+            <a className="absolute left-6 mt-4 text-stone-300">Norway</a>
             <p
-              className=" cursor-pointer h-6 w-6 bg-sky-800 rounded-full flex justify-center items-center transfor hover:scale-112 transition-transform duration-75"
+              className=" cursor-pointer h-6 w-6 mr-4 bg-sky-900 hover:bg-sky-700 rounded-full flex justify-center items-center transfor hover:scale-112 transition-transform duration-75"
               href="https://github.com/Stahelski"
               target="_blank"
               rel="noopener noreferrer"
@@ -120,7 +123,7 @@ export default function NewHero() {
               <img src="/Github.svg" alt="github logo" className="h-4.5"></img>
             </p>
             <p
-              className="cursor-pointer h-6 w-6 bg-sky-800 rounded-full flex justify-center items-center transfor hover:scale-112 transition-transform duration-75"
+              className="cursor-pointer h-6 w-6 bg-sky-900 hover:bg-sky-700 rounded-full flex justify-center items-center transfor hover:scale-112 transition-transform duration-75"
               href="#"
               target="_blank"
               rel="noopener noreferrer"
@@ -134,25 +137,23 @@ export default function NewHero() {
           </div>
 
           {/* Projects */}
+
           <a
-            className="cursor-pointer row-start-4 col-start-6 col-end-10 ml-2 pr-2 bg-[#100f14] text-stone-200 rounded-md flex justify-end items-center hover:bg-[#282630]"
+            className="cursor-pointer row-start-4 col-start-6 col-end-10 ml-2 m-1 pr-2 flex justify-end items-center bg-sky-950 text-stone-300 rounded-md border border-sky-300 hover:bg-sky-900 hover:border-sky-400 transition-colors group shadow-sm"
             href="/NewProjectPage"
           >
             Projects
           </a>
 
           {/* About Box */}
-          <div className="row-start-5 row-span-2 col-start-6 col-end-10 ml-2 flex items-center">
-            <a
-              className="cursor-pointer w-full h-[50%] bg-[#100f14] text-stone-200 rounded-md flex items-center pl-2 hover:bg-[#282630]"
-              href="/aboutPage"
-            >
-              About
-            </a>
-          </div>
-
+          <a
+            className="cursor-pointer row-start-5 col-start-6 col-end-10 ml-2 m-1 pr-2 flex justify-end items-center bg-sky-950 text-stone-300 rounded-md border border-sky-300 hover:bg-sky-900 hover:border-sky-400 transition-colors group shadow-sm"
+            href="/aboutPage"
+          >
+            About
+          </a>
           {/* Tic Tac Toe */}
-          <div className="cursor-pointer row-start-7 col-start-6 col-end-10 ml-2 pr-2 bg-[#100f14] text-stone-200 rounded-md flex justify-end items-center hover:bg-[#282630]">
+          <div className="cursor-pointer row-start-6 col-start-6 col-end-10 ml-2 m-1 pr-2 flex justify-end items-center bg-sky-950 text-stone-300 rounded-md border border-sky-300 hover:bg-sky-900 hover:border-sky-400 transition-colors group shadow-sm">
             Tick Tack Toe
           </div>
 
@@ -160,7 +161,7 @@ export default function NewHero() {
           <div className="row-start-9 col-start-2 col-end-7" />
 
           {/* Lifestyle Icons */}
-          <div className="row-start-10 col-start-2 col-end-7 text-stone-200 flex justify-between items-center pb-1">
+          <div className="row-start-10 col-start-2 col-end-7 text-stone-300 flex justify-between items-center pb-1">
             {lifestyle.map((item, index) => (
               <div
                 key={index}
--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,12 @@ import ProjectSection from "./components/organisms/projectSection";
 import Footer from "./components/organisms/Footer";
 import SilkWaveBackground from "./components/molecules/SilkWaveBackground";
 
+import FrontPage from "./NewHero/page.jsx";
+
 export default function Home() {
   return (
     <div className="overflow-x-scroll overflow-y-hidden">
-      <Hero />
-      <ProjectSection />
-      <Footer />
-      <SilkWaveBackground />
+      <FrontPage />
     </div>
   );
 }
