aa4e8a24f46bcd895615cd9b74cc13c74817c357
stian <stiankarlsen709@gmail.com>
2025-07-22 19:20:22 +0200

    TTT game update

--- src/app/TicTacToe/page.jsx
+++ src/app/TicTacToe/page.jsx
@@ -13,12 +13,40 @@ function Square({ value, onClick }) {
   );
 }
 
+function calcWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // for hver linje i "lines", ta og destrukturer array i "lines" => const [a, b, c] = lines[i];
+  // og hvist a er det samme som b og c, så har vi en vinner => "Her sjekker vi om alle tre rutene i en vinnerlinje har samme verdi."
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function displayWinner(calcWinner) {}
+
+function aiPlayer() {}
+
 export default function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(index) {
-    if (squares[index]) return; // Do nothing if already filled
+    if (squares[index] || calcWinner(squares)) {
+      return;
+    } // Do nothing if already filled, or return if there is a winner
 
     const nextSquares = squares.slice();
     nextSquares[index] = xIsNext ? "X" : "O";
@@ -26,8 +54,16 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calcWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next move belongs to " + (xIsNext ? "X" : "O");
+  }
+
   return (
-    <div className="w-full h-full flex justify-center items-center ">
+    <div className="w-full h-full flex justify-center items-center text-sky-300">
       <div className="Board w-full h-full grid grid-cols-3 row-span-3 bg-sky-950 border-2 rounded-md">
         {squares.map((value, index) => (
           <Square
@@ -37,6 +73,7 @@ export default function Game() {
           />
         ))}
       </div>
+      <div className="status absolute top-36">{status}</div>
     </div>
   );
 }
